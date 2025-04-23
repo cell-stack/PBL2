1c9ee46a5e7526cd65f439bf4180e9a4d205f8bd
cell-stack <cello.marcelino2735@gmail.com>
2025-04-23 19:32:16 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Laravel App</title>
+    <link rel="stylesheet" href="css/flowbite.min.css">
+    <script src="js/flowbite.min.js"></script>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-red-50">
+    <div class="container mx-auto mt-5 text-center p-3 rounded-lg shadow-md bg-white">
+        <div>
+            <p class="text-3xl capitalize"><span class="text-5xl capitalize font-norican">manies</span> 
+                cakery
+            </p>
+            <p class="text-gray-600">Delicious cakes made with love!</p>
+        </div>
+    </div>
+</body>
+</html>
