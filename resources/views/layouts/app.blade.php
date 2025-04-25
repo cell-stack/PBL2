0cf20c08665fbc22155af53d96293f5c929f98a0
cell-stack <cello.marcelino2735@gmail.com>
2025-04-26 00:11:08 +0700

    mmebuat struktur folder untuk views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,14 +14,22 @@
         <img src="assets/maniescakery2.png" alt="" class="w-40">
         <div class="text-white flex gap-15">
             <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">Home</a>
-            <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">Product</a>
+            <a href="catalog" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">Product</a>
             <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">About Us</a>
             <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">Login</a>
         </div>
     </header>
-    <main class="min-h-screen border mb-46">
+
+
+
+
+    <main class="min-h-screen mb-46">
         @yield('content')
     </main>
+
+
+
+
     <footer class="flex flex-col">
         <div class="bg-[#DFAC6B] flex items-center justify-between px-10 relative h-30 text-white font-bold">
             <div class="flex items-center gap-20">
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 @section('title', 'Homepage')
 @section('content')
-    <div class="container mx-auto flex flex-col items-center justify-center h-screen">
+    <div class="container mx-auto flex flex-col items-center justify-center h-screen bg-slate-400 
+    dark:bg-black dark:text-white">
         <div class="flex flex-col items-center justify-center gap-5">
             <h1 class="text-4xl font-bold">Welcome to Manies Cakery</h1>
             <p class="text-lg">Delicious cakes made with love.</p>
-            <a href="#" class="bg-[#DFAC6B] text-white py-2 px-4 rounded">Contoh gaessssss</a>
-        </div>  
+            <p class="font-norican">Delicious cakes made with love.</p>
+            @include('components.confirmModal')
+        </div>
     </div>
 @endsection
